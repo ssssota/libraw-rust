4e8a666fcabd86827773c565b94feab6f98a1813
ssssota <tommysouta@gmail.com>
2025-01-25 22:12:18 +0900

    fix: image data accessible

--- crates/libraw-rs/src/image.rs
+++ crates/libraw-rs/src/image.rs
@@ -1,36 +1,50 @@
-use libraw_sys::*;
+use std::borrow::Cow;
+
+use libraw_sys as sys;
 
 use crate::impl_property;
 
 pub struct ProcessedImage {
-    inner: libraw_processed_image_t,
+    inner: sys::libraw_processed_image_t,
 }
 
 impl Drop for ProcessedImage {
     fn drop(&mut self) {
         let ptr = &mut self.inner as *mut _;
-        unsafe { libraw_dcraw_clear_mem(ptr) };
+        unsafe { sys::libraw_dcraw_clear_mem(ptr) };
     }
 }
 
 impl ProcessedImage {
-    pub(crate) fn new(inner: libraw_processed_image_t) -> Self {
+    pub(crate) fn new(inner: sys::libraw_processed_image_t) -> Self {
         ProcessedImage { inner }
     }
 
-    impl_property!(r#type, type_, LibRaw_image_formats);
+    pub fn r#type(&self) -> ImageFormat {
+        ImageFormat::from(self.inner.type_)
+    }
     impl_property!(height, u16);
     impl_property!(width, u16);
     impl_property!(colors, u16);
     impl_property!(bits, u16);
     impl_property!(data_size, u32);
-    impl_property!(data, [u8; 1usize]);
+    pub fn data(&self) -> Cow<[u8]> {
+        let data = &self.inner.data as *const _;
+        let len = self.inner.data_size as usize;
+        Cow::Borrowed(unsafe { std::slice::from_raw_parts(data, len) })
+    }
 }
 
-// pub type_: LibRaw_image_formats,
-// pub height: ushort,
-// pub width: ushort,
-// pub colors: ushort,
-// pub bits: ushort,
-// pub data_size: libc::c_uint,
-// pub data: [libc::c_uchar; 1usize],
+pub enum ImageFormat {
+    JPEG = sys::LibRaw_image_formats_LIBRAW_IMAGE_JPEG as isize,
+    BITMAP = sys::LibRaw_image_formats_LIBRAW_IMAGE_BITMAP as isize,
+}
+impl From<sys::LibRaw_image_formats> for ImageFormat {
+    fn from(value: sys::LibRaw_image_formats) -> Self {
+        match value {
+            sys::LibRaw_image_formats_LIBRAW_IMAGE_JPEG => ImageFormat::JPEG,
+            sys::LibRaw_image_formats_LIBRAW_IMAGE_BITMAP => ImageFormat::BITMAP,
+            _ => unreachable!(),
+        }
+    }
+}
